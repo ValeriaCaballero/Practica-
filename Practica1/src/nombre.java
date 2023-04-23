5229cc1aed68924b0909689f5b7688d81ab8109f
Valeria <A18003354@alumnos.uady.mx>
2023-04-23 17:02:38 -0600

    Se agrego un nuevo comentario al .java

--- Practica1/src/nombre.java
+++ Practica1/src/nombre.java
@@ -5,6 +5,8 @@ public class nombre {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Valeria Isabel Caballero Góngora"); 
+		
+		System.out.println("Este es otro comentario para volver a subir este .java al repositorio remoto"); 
 
 	}
 
